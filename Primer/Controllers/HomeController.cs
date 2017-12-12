0525edb099a6516e674643b82bcb8b77f4b52202
engageflopes <filipe.lopes@engage.bz>
2017-12-12 13:19:56 -0200

    p42

---
+++ Primer/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Primer.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
