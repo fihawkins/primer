cf19b5dfcdf3f2931717767d3cad00a1a7c8d6e2
engageflopes <filipe.lopes@engage.bz>
2017-12-23 15:21:04 -0200

    cap 3

--- Primer/Controllers/PageSizeController.cs
+++ Primer/Controllers/PageSizeController.cs
@@ -1,20 +1,32 @@
-﻿using System.Diagnostics;
+﻿using Primer.Infrasctructure;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System;
 
 namespace Primer.Controllers
 {
-    public class PageSizeController : ApiController
+    public class PageSizeController : ApiController, ICustomController
     {
         private static string TargetUrl = "https://www.amazon.com/";
 
-        public long GetPageSize()
+        public async Task<long> GetPageSize(CancellationToken cToken)
         {
             WebClient wc = new WebClient();
             Stopwatch sw = Stopwatch.StartNew();
-            byte[] apressData = wc.DownloadData(TargetUrl);
+            byte[] amazonData = await wc.DownloadDataTaskAsync(TargetUrl);
             Debug.WriteLine("Elapsed ms: {0}", sw.ElapsedMilliseconds);
-            return apressData.LongLength;
+            return amazonData.LongLength;
+        }
+
+        public Task PostUrl(string newUrl, CancellationToken cToken)
+        {
+            TargetUrl = newUrl;
+            return Task.FromResult<object>(null);
         }
     }
 }
